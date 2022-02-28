f03ebdfdc479859d0d318ffe29d4f1a09b1255d0
XiangweiLv <lxw2089@163.com>
2022-02-28 15:29:59 +0800

    initial naive_bayes

--- 4.naive_Bayes/bayes.py
+++ 4.naive_Bayes/bayes.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def load_dataset():
     postingList=[['my', 'dog', 'has', 'flea', \
     'problems', 'help', 'please'],
@@ -24,3 +26,28 @@ def set_of_words2vec(vocab_list, input_set):
         else:
             print(f'the word {word} is not in my Vocabulary!')
     return return_vec
+
+def train_naive_Bayes0(train_mat, train_category):
+    '''
+    train_mat: (number of sentences, number of vocab)
+    train_category: (number of sentences, ) 1 indicates abusive
+    '''     
+
+    num_train_docs, num_of_vocab = len(train_mat), len(train_mat[0])
+    p_abusive = sum(train_category) / num_train_docs
+
+    p0_num = np.zeros(num_of_vocab)
+    p1_num = np.zeros(num_of_vocab)
+    p0_denom = p1_denom = 0.0
+
+    for i in range(num_train_docs):
+        if train_category[i] == 1:
+            p1_num += train_mat[i]
+            p1_denom += sum(train_mat[i])
+        else:
+            p0_num += train_mat[i]
+            p0_denom += sum(train_mat[i])
+    p1_vec = p1_num / p1_denom
+    p0_vec = p0_num / p0_denom
+
+    return p0_vec, p1_vec, p_abusive                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
